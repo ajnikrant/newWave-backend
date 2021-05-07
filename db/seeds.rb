c3d770fd57da2a8738ead12fadfd6ba0e452eb65
Amanda <ajnikrant@gmail.com>
2021-05-07 15:30:10 -0400

    seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,13 @@ end
 puts "seeded users"
 
 10.times do
-    Listing.create!(title: Faker::Music.instrument, location: Faker::Games::Pokemon.location, price: Faker::Number.within(range: 1..1000), description: Faker::Movies::StarWars.quote, barter: Faker::Boolean.boolean, for_sale: Faker::Boolean.boolean, category: Faker::Music.genre, user_id: User.all.sample)
+    Listing.create!(title: Faker::Music.instrument, location: Faker::Games::Pokemon.location, price: Faker::Number.within(range: 1..1000), description: Faker::Movies::StarWars.quote, barter: Faker::Boolean.boolean, for_sale: Faker::Boolean.boolean, category: Faker::Music.genre, user_id: User.all.sample.id)
 end
 puts "seeded listings"
 
 
 10.times do
-    Sale.create!(user_id: User.all.sample, listing_id: Listing.all.sample, price: Faker::Number.within(range: 1..1000))
+    Sale.create!(user_id: User.all.sample.id, listing_id: Listing.all.sample.id, price: Faker::Number.within(range: 1..1000))
 end
 puts "seeded sales"
 
