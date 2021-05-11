278d962c2a1e4d026890f898da040fc3157b9601
Melissa Lawrence <mlawrence537@gmail.com>
2021-05-11 11:27:04 -0400

    added to listing serializer

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,3 +1,3 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :price, :title, :description, :user_id, :category, :barter, :for_sale, :barter_description, :image, :location
 end
